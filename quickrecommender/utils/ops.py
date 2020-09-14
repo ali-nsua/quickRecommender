58e0f9e96fe28feb23a8cc2e259717229287788f
Ali Hassani <68103095+ali-nsua@users.noreply.github.com>
2020-09-14 14:33:17 +0430

    Minor fixes

--- quickrecommender/utils/ops.py
+++ quickrecommender/utils/ops.py
@@ -21,7 +21,7 @@ def cosine_similarity(X, Y=None):
         return ((1 + np.dot(X_normalized, X_normalized.T)) / 2), X_normalized
     Y_normalized = normalize(Y)
     K = np.dot(X_normalized, Y_normalized.T)
-    return ((1 + K) / 2), X_normalized
+    return (1 + K) / 2
 
 
 def softmax(x):
